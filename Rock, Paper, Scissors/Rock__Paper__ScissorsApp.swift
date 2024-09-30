5986563f81f04f0bf19b41579c50c790e9d4e4ab
Anna Filin <68apple@gmail.com>
2024-09-30 15:15:19 +0300

    Initial Commit

---
+++ Rock, Paper, Scissors/Rock__Paper__ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock__Paper__ScissorsApp.swift
+//  Rock, Paper, Scissors
+//
+//  Created by Anna Filin on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rock__Paper__ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
